95d436e9318b19eaa934fe97d220f4cc97776ef0
vbm <vaishu16ab@gmail.com>
2022-06-16 11:19:20 +0530

    Understanding matrix and to execute a python script

--- Scripts/sample.py
+++ Scripts/sample.py
@@ -1,1 +1,5 @@
-print('Thanks for creating a pull request')
+print('Demo file to get familiar on how to execute a python script in github workflow')
+a=10
+b=20
+sum=a+b
+print(f'Sum of two values {0} and {1} is {2}'.format(a , b, sum))
